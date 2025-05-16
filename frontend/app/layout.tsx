175e93987490d07a38c7dcef701dc430f2849720
Aleksander Baska <baskaaleksander03@gmail.com>
2025-05-16 17:02:16 +0200

    feat(auth): implement AuthProvider with login, logout, and register functionality; add axios utility for API calls

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { AuthProvider } from "@/providers/auth-provider";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -18,12 +19,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body
+          className={`${inter.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </AuthProvider>
   );
 }
---
+++ frontend/providers/auth-provider.tsx
@@ -0,0 +1,88 @@
+import api from "@/utils/axios";
+import { createContext, useContext, useState } from "react";
+
+interface AuthContextType {
+    user: UserData | null;
+    loading: boolean;
+    login: (email: string, password: string) => Promise<void>;
+    logout: () => Promise<void>;
+    register: (email: string, password: string) => Promise<void>;
+}
+
+interface UserData {
+    email: string;
+    id: string;
+    role: string;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+    const [user, setUser] = useState<any | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    const login = async (email: string, password: string) => {
+        setLoading(true);
+        try {
+            const user = await api.post("/auth/login", {
+                email,
+                password,
+            });
+            setUser({
+                email: user.data.email,
+                id: user.data.id,
+                role: user.data.role,
+            });
+        } catch (error) {
+            console.error("Login failed", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const logout = async () => {
+        setLoading(true);
+        try {
+            await api.post("/auth/logout");
+            setUser(null);
+        } catch (error) {
+            console.error("Logout failed", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const register = async (email: string, password: string) => {
+        setLoading(true);
+        try {
+            const user = await api.post("/auth/register", {
+                email,
+                password,
+            });
+            setUser({
+                email: user.data.email,
+                id: user.data.id,
+                role: user.data.role,
+            });
+        } catch (error) {
+            console.error("Register failed", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, loading, login, logout, register }}>
+            {children}
+        </AuthContext.Provider>
+    );
+    
+}
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
